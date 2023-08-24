0d96ad85cfe88ac5240ee5854802295a4ffa761b
keurid <kevindesantiago99@gmail.com>
2023-08-23 19:02:38 -0700

    added serverjs and basic code so that i can run

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
