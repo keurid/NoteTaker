76ca17f231f736bef074a72dda2239a4a24a8464
keurid <kevindesantiago99@gmail.com>
2023-08-23 19:19:55 -0700

    added the basic gets and post

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+let notes = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
